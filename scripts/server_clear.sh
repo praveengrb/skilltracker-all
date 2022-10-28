3d5cb5d07e4d29f1d49e1ee7d7e90e74ff7afa9d
Praveen <praveengrb@gmail.com>
2022-10-28 18:59:45 +0530

    full code

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -10,7 +10,7 @@ sudo yum -y install wget
 
 sudo service codedeploy-agent stop
 cd /home/ec2-user
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+wget https://xxxxx/latest/install
 chmod +x ./install
 sudo ./install auto
 
